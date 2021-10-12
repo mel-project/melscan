56b5e0cb2cafbaa6df6765e849ddf5fdb48f74e4
Shane Mendez <mendezsoftware@gmail.com>
2021-10-12 17:34:33 -0500

    blocks auto update

--- src/main.rs
+++ src/main.rs
@@ -65,8 +65,8 @@ async fn main_inner() -> anyhow::Result<()> {
     // Raw paths
     app.at("/raw/latest").get(raw::get_latest);
     app.at("/raw/blocks/:height").get(raw::get_header);
-    app.at("/raw/blocks/:height/full").get(raw::get_full_block);
     app.at("/raw/blocks/:height/summary").get(raw::get_block_summary);
+    app.at("/raw/blocks/:height/full").get(raw::get_full_block);
     app.at("/raw/blocks/:height/transactions/:txhash")
         .get(raw::get_transaction);
     app.at("/raw/blocks/:height/coins/:coinid")
