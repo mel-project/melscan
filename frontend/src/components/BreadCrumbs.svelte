0cffe482ae127ab27a1ba408c9ee4a0bf20da4fc
Shane Mendez <mendezsoftware@gmail.com>
2022-07-11 16:04:22 -0500

    added breadcrumbs

---
+++ frontend/src/components/BreadCrumbs.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { BreadCrumb } from "@utils/page-types";
+
+
+    export let breadcrumbs: BreadCrumb[]
+</script>
+
+<template>
+    <div class="breadcrumbs">
+        {#each breadcrumbs as breadcrumb,index}
+            <a class="breadcrumb" href={breadcrumb.href}>{breadcrumb.title}</a>
+            {#if index + 1 !== breadcrumbs.length}🢒
+            {/if}
+        {/each}
+    </div>
+</template>
+
+<style lang="scss">
+    .breadcrumbs{
+    }
+    .breadcrumb{
+        color: gray;
+        margin: 0 .1em 0 .3em;
+    }
+    .breadcrumb:last-child{
+        color: black;
+    }
+</style>
