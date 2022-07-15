e568668d4319ac4758a6a0fc244b19a64c7dbff0
nullchinchilla <nullchinchilla@pm.me>
2022-07-15 13:14:34 -0500

    universal graph helpers

--- backend/src/graphs.rs
+++ backend/src/graphs.rs
@@ -1,1 +1,37 @@
+use chrono::Utc;
+use futures_util::Future;
+use itertools::Itertools;
+use serde::{Deserialize, Serialize};
+use themelio_structs::{BlockHeight, Denom, PoolKey};
+
 mod helpers;
+
+/// A JSON-friendly graphing datum
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+pub struct GraphDatum {
+    pub height: BlockHeight,
+    pub date: chrono::DateTime<Utc>,
+    pub value: f64,
+}
+
+/// Given a range of blocks, and a function that resolves a value given a block height, return a bunch of graph data.
+pub async fn graph_range<F: Future<Output = anyhow::Result<GraphDatum>> + Send + Sync + 'static>(
+    start: BlockHeight,
+    end: BlockHeight,
+    approx_count: usize,
+    resolve: impl Fn(BlockHeight) -> F,
+    cached: impl Fn(BlockHeight) -> Option<GraphDatum>,
+) -> anyhow::Result<Vec<GraphDatum>> {
+    let heights = helpers::interpolate_between(start.0, end.0, approx_count as _).collect_vec();
+    helpers::fast_async_map(heights, |height| {
+        let height = BlockHeight(height);
+        if let Some(cached) = cached(height) {
+            Ok(Ok(cached))
+        } else {
+            Err(smolscale::spawn(resolve(height)))
+        }
+    })
+    .await
+    .into_iter()
+    .collect()
+}
--- backend/src/graphs/helpers.rs
+++ backend/src/graphs/helpers.rs
@@ -1,27 +1,27 @@
+use futures_util::stream::FuturesUnordered;
 use futures_util::StreamExt;
-use futures_util::{stream::FuturesUnordered, Future};
 use itertools::Itertools;
+use smol::Task;
 use std::collections::BTreeMap;
 
 /// Interpolates between two numbers in a cache-friendly fashion
 pub fn interpolate_between(start: u64, end: u64, approx_count: u64) -> impl Iterator<Item = u64> {
     let interval = ((end - start).max(1) / approx_count).next_power_of_two();
-    (start..=end)
-        .filter(move |i| i % interval == 0)
-        .chain(std::iter::once(end))
-        .unique()
+    (start..=end).filter(move |i| i % interval == 0).unique()
 }
 
-/// Efficiently map an asynchronous function over a vector, doing any concurrency only when any of the asynchronous functions "block".
-pub async fn fast_async_map<T, U, F: Future<Output = U>>(v: Vec<T>, f: impl Fn(T) -> F) -> Vec<U> {
+/// Efficiently map an asynchronous function over a vector, doing any concurrency only when any of the functions need to block (indicated by returning a task instead of an immediate value).
+pub async fn fast_async_map<T, U>(v: Vec<T>, f: impl Fn(T) -> Result<U, Task<U>>) -> Vec<U> {
     let mut toret = BTreeMap::new();
     let mut pending = FuturesUnordered::new();
     for (i, val) in v.into_iter().enumerate() {
-        let mut fut = f(val);
-        if let Some(res) = smol::future::poll_once(&mut fut).await {
-            toret.insert(i, res);
-        } else {
-            pending.push(async move { (i, fut.await) });
+        match f(val) {
+            Ok(res) => {
+                toret.insert(i, res);
+            }
+            Err(fut) => {
+                pending.push(async move { (i, fut.await) });
+            }
         }
     }
     while let Some((i, v)) = pending.next().await {
@@ -37,10 +37,17 @@ mod tests {
     #[test]
     fn fam_simple() {
         assert_eq!(
-            smol::future::block_on(fast_async_map(
-                vec![1, 2, 3, 4, 5],
-                |x| async move { x * 2 }
-            )),
+            smol::future::block_on(fast_async_map(vec![1, 2, 3, 4, 5], |x| Ok(x * 2))),
+            vec![2, 4, 6, 8, 10]
+        )
+    }
+
+    #[test]
+    fn fam_blocking() {
+        assert_eq!(
+            smol::future::block_on(fast_async_map(vec![1, 2, 3, 4, 5], |x| Err(smol::spawn(
+                async move { x * 2 }
+            )))),
             vec![2, 4, 6, 8, 10]
         )
     }
