092c2c57d0498cbea936b99b6a5f24133a683604
nullchinchilla <nullchinchilla@pm.me>
2022-08-02 12:03:02 -0500

    fix

--- dev.sh
+++ dev.sh
@@ -2,7 +2,7 @@
 
 export VITE_BASE_URL=http://127.0.0.1:13000
 
-cd backend && find src | entr -r cargo run   --release --locked --target x86_64-unknown-linux-musl -- --connect 127.0.0.1:11814 --listen 127.0.0.1:13000 --blkidx-db test.db &
+cd backend && find src | entr -r cargo run   --release --locked --target x86_64-unknown-linux-musl -- --connect 194.182.170.249:11814 --listen 127.0.0.1:13000 --testnet &
 # kill the background process when we exit
 trap "sh -c 'killall entr; killall melscan-backend'" INT TERM EXIT
 
